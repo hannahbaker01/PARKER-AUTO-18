b3c50c67784c6cd0274d8ae2ca1ebc2be53d9c74
hannahbaker01 <134852510+hannahbaker01@users.noreply.github.com>
2023-10-26 22:12:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ MELCOW_IMG = environ.get('MELCOW_IMG',"https://telegra.ph/file/e54cae941b9b81f13
 MELCOW_VID = environ.get('MELCOW_VID',"")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5596825598').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6315836859 5840622348 6067718572 5023815012 6441914464').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001793383615 -1001921845518').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
